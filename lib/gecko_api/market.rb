0b43734ee5df6fc510cb4dfc87b0605a00e38edf
nolyoi <nolan@moonship.cc>
2020-05-07 20:09:18 -0500

    Added Art

--- lib/gecko_api/market.rb
+++ lib/gecko_api/market.rb
@@ -5,7 +5,7 @@
 # markets?vs_currency=usd
 
 class Market
-	attr_accessor :id, :name, :symbol, :price, :price_movement_24h, :market_cap_rank
+	attr_accessor :id, :name, :symbol, :price, :price_movement_24h, :market_cap_rank, :image
 	BASE_URL =- "https://api.coingecko.com/api/v3/coins/"
 	@@market = []
 	@@list = []
@@ -16,9 +16,10 @@ class Market
 
 		# looping until we hit the end of the list. adding them all as objects.
 		while i < data.length
-			@id = data[i]["id"].to_s
-			@name = data[i]["name"].to_s
-			@symbol = data[i]["symbol"].to_s
+			@id = data[i]["id"]
+			@name = data[i]["name"]
+			@symbol = data[i]["symbol"]
+			@image = data[i]["image"]
 			@price = data[i]["current_price"].to_s
 			@price_movement_24h = data[i]["price_change_percentage_24h"]
 			@market_cap_rank = data[i]["market_cap_rank"]
@@ -40,21 +41,25 @@ class Market
 
 	def self.top
 		i = 0
+		rows = []
 		@@market.each do |coin|
 			if coin.price_movement_24h > 0
-				puts "#{i +1}. #{coin.name} (#{coin.symbol.upcase}) - $#{coin.price.colorize(:green)}"
+				rows << ["#{i +1}", "#{coin.name}", "#{coin.symbol.upcase}", "$#{coin.price.colorize(:green)}"]
 				i += 1
 			else
-				puts "#{i +1}. #{coin.name} (#{coin.symbol.upcase}) - $#{coin.price.colorize(:red)}"
+				rows << ["#{i +1}", "#{coin.name}", "#{coin.symbol.upcase}", "$#{coin.price.colorize(:red)}"]
 				i += 1
 			end
 		end
+		table = Terminal::Table.new :headings => ['Rank', 'Name', 'Symbol', 'Current Price'], :rows => rows
+		puts table
 		puts "==========================================="
 		puts "Type a coin ticker, ex. 'BTC', for more information or 'back' to return to the main menu."
 		Controller.menu
 	end
 
 	def self.coin(number)
+		a = Artii::Base.new :font => 'slant'
 		puts "Searching"
 		@@market.each do |coin|
 			if coin.market_cap_rank.to_s == number
@@ -62,6 +67,7 @@ class Market
 				data = JSON.parse(open(BASE_URL + id).read)
 				Controller.clear_term
 				if data["market_data"]["price_change_percentage_24h"] > 0
+					puts a.asciify(data["name"])
 					print "#{data["name"].colorize(:red)} (#{data["symbol"].upcase.colorize(:red)}) " + "$".colorize(:green) + "#{data["market_data"]["current_price"]["usd"].to_s.colorize(:green)} - #{data["market_data"]["price_change_percentage_24h"].round(2).to_s.colorize(:green)}" + "%".colorize(:green)
 				else
 					print "#{data["name"].colorize(:red)} (#{data["symbol"].upcase.colorize(:red)}) " + "$".colorize(:green) + "#{data["market_data"]["current_price"]["usd"].to_s.colorize(:red)} - #{data["market_data"]["price_change_percentage_24h"].round(2).to_s.colorize(:red)}" + "%".colorize(:red)
@@ -77,7 +83,8 @@ class Market
 				# formatting the marketcap number. taking the first 6 numbers of the string.
 				puts " "
 				puts "Total Market Cap: $#{data["market_data"]["market_cap"]["usd"].to_s[0..5].insert(-4, ".")} #{mktcap}"
-
+				puts " "
+				puts "Description:".colorize(:red)
 				# project description formatting. removing HTML elements but keeps links within parentheses
 				description = data["description"]["en"]
 				puts description.gsub(/<[^"\\] href="/, '(').gsub(/["\\]>/, ') ').gsub(/<[^<\\]a>/, '')
